738aa4aba30356933224ad84c15f1076b668329d
Derek Smyth <smyth.derek@gmail.com>
2023-01-17 22:16:26 -0500

    commit tree 9cdd5fba9ecd43f54e7cabfce67ea424f6449549

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Derek Smyth", 
+    streetAddress: "112 Dunnlea Rd",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    const objNew = {...obj};
+    delete objNew[key];
+    return objNew;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+};
